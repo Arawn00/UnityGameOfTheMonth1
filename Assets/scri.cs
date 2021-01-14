2ae147f512961fea0c0d793e9c77e40f7e7d8f4c
Anthony <anthony-the-boss@hotmail.com>
2021-01-14 20:53:39 +0100

    Premier push du jeu unity

---
+++ Assets/scri.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scri : MonoBehaviour
+{
+    life = 3;
+    public int cooldown = 10;
+    public float cooldownup = 0.0f;
+    private int add = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldown += cooldownup;
+    }
+}
